cf3c02ed802953340cab8cf2171d545a79080cce
ABHISHEK KUMAR <107021877+imabhishekmahli@users.noreply.github.com>
2022-06-26 23:01:18 +0530

    Update 3.Creating Table.sql

--- sql programming/3.Creating Table.sql
+++ sql programming/3.Creating Table.sql
@@ -1,8 +1,6 @@
 
 
 create table canon_cameras( 
-							model_name varchar(30),
-                            quantity int 
-                            );
-				
+			model_name varchar(30),
+                        quantity int );
 desc canon_cameras;
