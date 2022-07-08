ee2ba2912f4c64c010e0804f9a77e860e5c56008
ABHISHEK KUMAR <107021877+imabhishekmahli@users.noreply.github.com>
2022-07-08 18:06:53 +0530

    Add files via upload

---
+++ sql programming/SQL - Part 11 - Order By Clause (ASC, DESC).sql
@@ -0,0 +1,11 @@
+-- ## to ordered the retrieved data in ascending or descending order 
+-- ## ASC AND DESC
+
+-- SHOW DATABASES;
+-- USE COLLEGE;
+
+-- SHOW TABLES;
+
+-- SELECT * FROM college.student;
+
+SELECT * FROM STUDENT ORDER BY NAME ASC,ADDRESS ASC;
