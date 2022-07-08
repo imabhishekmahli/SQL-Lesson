ba1bb4f172a9c53bdcbf2277e7bf9875ef66ce33
ABHISHEK KUMAR <107021877+imabhishekmahli@users.noreply.github.com>
2022-07-08 17:33:24 +0530

    Add files via upload

---
+++ sql programming/Between Operator.sql
@@ -0,0 +1,12 @@
+-- ## To filter the records in the specified range
+-- show databases;
+-- use college;
+
+-- show tables;
+
+-- select * from college.student;
+
+select * from student 
+where id between 2 and 3;
+
+-- where columns>=$$ and/or columns<=$$
